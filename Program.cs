489c7b6332b9b74f69015d1240a686dc57b83e27
DariaParova <114743061+DariaParova@users.noreply.github.com>
2022-11-29 22:52:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// 
+
+string[] arrayone = new string[4] {"hello", "2", "world", ":-)"};
+string[] arraytwo = new string[arrayone.Length];
+void SecondArrayWithIF(string[] arrayone, string[] arraytwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayone.Length; i++)
+    {
+    if(arrayone[i].Length <= 3)
+        {
+        arraytwo[count] = arrayone[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arrayone, arraytwo);
+PrintArray(arraytwo);
